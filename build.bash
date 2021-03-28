afd0481ecd44c6993e36a3ca6155ec992911314c
tahvnon <tahvovirtanen+tahvnon@gmail.com>
2021-03-28 23:23:21 +0300

    build script

---
+++ build.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+OUTFILE='index.html'
+cd video
+echo > $OUTFILE
+echo '<html><head><title>videos</title></head>' >> $OUTFILE
+echo '<body>' >> $OUTFILE
+
+echo '<div>' >> $OUTFILE
+echo '<ul>' >> $OUTFILE
+
+for file in `ls`
+do
+    echo "<li><a href=\"$file\">$file</a></li>" >> $OUTFILE
+done
+echo '</ul>' >> $OUTFILE
+echo '</body></html>' >> $OUTFILE
+
